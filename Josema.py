e5216ac01102df75fda3d32d0480c94242be3b90
joseM-21T <A01733838@tec.mx>
2025-04-07 14:03:47 -0600

    Jerarquía ejemplo

---
+++ Josema.py
@@ -0,0 +1,7 @@
+payaso = 112
+muñeca = 75
+
+merc1 = float(input("peso del payaso: "))
+merc2 = float(input("peso de muñeca: "))
+
+print("El total del paquete de payaso y muñeca es {}".format(((payaso * merc1) + (muñeca * merc2))))
